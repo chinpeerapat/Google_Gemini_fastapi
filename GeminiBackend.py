42635f70c503d1164708a1d194c4e47e7348dd4d
Anamol <anmoldkl971@gmail.com>
2024-01-19 18:43:54 +0545

    Version 1.1

--- GeminiBackend.py
+++ GeminiBackend.py
@@ -1,15 +1,10 @@
-#AIzaSyDxs0jRMN67R0FGYb36I_cF0HOvbdJTn0Y
-
-import io
-import textwrap
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 import google.generativeai as genai
-import requests
-from PIL import Image
+import markdown
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -18,12 +13,26 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 origins = ["http://127.0.0.1:8000"]
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    CORSMiddleware, 
+    allow_origins=origins, 
+    allow_credentials=True, 
+    allow_methods=["*"], 
+    allow_headers=["*"]
 )
+
+def GenerativeModel():
+    genai.configure(api_key="AIzaSyDxs0jRMN67R0FGYb36I_cF0HOvbdJTn0Y")
+    return genai.GenerativeModel('gemini-pro')
+
+def convert_to_markdown(paragraph):
+    return markdown.markdown(paragraph)
+
+def removeComments(text):
+    lines = text.split('\n')
+    filtered_lines = [line.split("#")[0] if "#" in line else line for line in lines]
+    result_text = '\n'.join(filtered_lines)
+    result_text = result_text.replace("```","").replace("python","")
+    return result_text
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
@@ -34,19 +43,11 @@ async def favicon():
     pass
 
 @app.get("/gemini")
-async def query(query: str):
-    genai.configure(api_key="AIzaSyDxs0jRMN67R0FGYb36I_cF0HOvbdJTn0Y")
-    model = genai.GenerativeModel('gemini-pro')
+async def query(query: str, model: GenerativeModel = Depends(GenerativeModel)):
+    if not query:
+        return ''
     response = model.generate_content(query)
-    text = response.text.replace('•', '  *')
-    return text
-
-@app.get("/gemini/img")
-async def queryimg(query: str, image_url: str):
-    genai.configure(api_key="AIzaSyDxs0jRMN67R0FGYb36I_cF0HOvbdJTn0Y")
-    response = requests.get(image_url)
-    img = Image.open(io.BytesIO(response.content))
-    model = genai.GenerativeModel('gemini-pro-vision')
-    response = model.generate_content([query, img], stream=True)
-    response.resolve()
-    return response.text
+    text = response.text.replace('•', '  *').replace("*", "<i>").replace("python", "")
+    html_code = convert_to_markdown(removeComments(text))
+    python_code = html_code.replace("```", "").replace("<h1>", "<h7>").replace("</h1>", "</h7>")
+    return python_code
--- static/script.js
+++ static/script.js
@@ -7,10 +7,18 @@ function handleFormSubmit() {
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
-            return response.json();
+            return response.text();
         })
         .then(data => {
-            userInputDisplay.innerHTML += `<div class="message"><br><br>${userInput}<br>${data}<br><br></div>`;
+            const messageElement = document.createElement('div');
+            messageElement.classList.add('message');
+            
+            // const converter = new showdown.Converter();
+            // const htmlContent = converter.makeHtml(data);
+
+            messageElement.innerHTML = data;
+
+            userInputDisplay.appendChild(messageElement);
         })
         .catch(error => {
             console.error(error);
@@ -20,45 +28,16 @@ function handleFormSubmit() {
     userInputDisplay.scrollTop = userInputDisplay.scrollHeight;
 }
 
-function handleImageUpload(event) {
-    const fileInput = event.target;
-    const files = fileInput.files;
-
-    if (files.length > 0) {
-        const image = files[0];
-        const imageURL = URL.createObjectURL(image);
-
-        const userInputDisplay = document.getElementById('user-input-display');
-
-        fetch(`http://localhost:8000/gemini/img?queryimg=${encodeURIComponent(userInput)}&image_url=${encodeURIComponent(imageURL)}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then(data => {
-                userInputDisplay.innerHTML += `<div class="message"><br><br><img src="${imageURL}" alt="User Image" style="max-width: 550px; max-height: 750px;"></div><br>${data}`;
-            })
-            .catch(error => {
-                console.error(error);
-            });
-
-        userInputDisplay.scrollTop = userInputDisplay.scrollHeight;
-        document.getElementById('image-input').value = '';
-    }
-}
-
 function handleKeyPress(event) {
     if (event.key === 'Enter') {
         event.preventDefault();
+        
+        const userInput = document.getElementById('name-input').value;
 
-        const targetId = event.target.id;
-
-        if (targetId === 'name-input') {
+        if (userInput.trim().length > 0) {
             handleFormSubmit();
-        } else if (targetId === 'image-input' && event.target.value.trim() !== '') {
-            handleImageUpload(event);
+        } else {
+            console.log("Input is empty. Not doing the work.");
         }
     }
 }
